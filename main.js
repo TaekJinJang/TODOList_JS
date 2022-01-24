96cc40fd10993d7db9d591cfb35680baf000f5c3
Taekjin Jang <xorwls1500@naver.com>
2022-01-24 16:32:09 +0900

    ing

--- main.js
+++ main.js
@@ -4,3 +4,30 @@
 // check  버튼을 누르면 할 일이 끝나면서 문자에 밑줄을 침
 // Not done을 누르면 언더바 이동
 // done은 done 할 일만 나온다
+
+let taskInput = document.getElementById("task-input");
+let btnAdd = document.getElementById("add");
+let taskList = [];
+
+btnAdd.addEventListener("click", addTask);
+
+function addTask() {
+  let taskContent = taskInput.value;
+  taskList.push(taskContent);
+  render();
+}
+
+function render() {
+  let resultHTML = "";
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `<div class="task">
+    <div>${taskList[i]}</div>
+      <div>
+          <button>Check</button>
+          <button>Delete</button>
+      </div>
+  </div>`;
+  }
+
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
